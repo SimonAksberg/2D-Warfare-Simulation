d286b1e55e52d4c7e211662bdd567d9c23e511fd
SimonAksberg <simon.aksberg@gmail.com>
2026-07-03 12:02:43 +0200

    Add core structure to allow rendering of the simulation window (closes #1, closes #3)

--- src/simulation.py
+++ src/simulation.py
@@ -1,4 +1,35 @@
 import pygame
+import constants
+
+class Simulation():
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode(
+            (constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT)
+        )
+
+        pygame.display.set_caption("Warfare Simulation")
+
+        self.running = True
+
+    def run(self):
+        # Simulation loop
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # Fill screen with background color to wipe away anything from previous frame
+            self.screen.fill(constants.BACKGROUND_COLOR)
+
+            # This is where the actual simulation should take place
 
 
-        
+            # Updates window based on what has happened in game loop
+            pygame.display.flip()
+
+        pygame.quit()
+
+
+
